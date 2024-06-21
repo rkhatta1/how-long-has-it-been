c5c6b3d8c042f9abc83b4669bd05cb32ce4f057f
rkhatta1 <rkhatta1@asu.edu>
2024-06-21 12:08:17 -0700

    hardcoded the api key

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: isProd ? '/how-long-has-it-been' : '',
-  assetPrefix: isProd ? '/how-long-has-it-been/' : '',
-  // Other Next.js configuration options can go here
 };
 
 module.exports = nextConfig;
